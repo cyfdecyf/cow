41635063013d92a93d61b4f9afd8a12fd8839b26
Chen Yufei <cyfdecyf@gmail.com>
2012-09-26 16:21:24 +0800

    Add profiling.

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+)
+
+var c = make(chan os.Signal, 1)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			for sig := range c {
+				info.Printf("captured %v, stopping profiler and exiting..", sig)
+				pprof.StopCPUProfile()
+				os.Exit(0)
+			}
+		}()
+	}
+
 	py := NewProxy("localhost:9000")
 	py.Serve()
 }
