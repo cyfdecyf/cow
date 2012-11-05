2cc9cdbb53297c9cf5d7c6b5c62d7475db0ff6c4
Chen Yufei <cyfdecyf@gmail.com>
2012-11-05 15:03:25 +0800

    Simple config file parsing.

--- config.go
+++ config.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"os/user"
 	"path"
+	// "reflect"
+	"strconv"
 	"strings"
 )
 
@@ -14,15 +17,17 @@ var homeDir string
 const (
 	dotDir       = ".cow-proxy"
 	blockedFname = "blocked"
+	rcFname      = "rc"
 )
 
 var config struct {
 	listenAddr string // server listen address
+	socksAddr  string
 	numProc    int // max number of cores to use
 
 	dir         string // directory containing config file and blocked site list
-	socksAddr   string
 	blockedFile string
+	rcFile      string
 }
 
 func init() {
@@ -33,20 +38,27 @@ func init() {
 	}
 	homeDir = u.HomeDir
 
-	config.listenAddr = "127.0.0.1:8087"
+	config.listenAddr = "127.0.0.1:7777"
 	config.numProc = 2
-
 	config.socksAddr = "127.0.0.1:1080"
+
 	config.dir = path.Join(homeDir, dotDir)
 	config.blockedFile = path.Join(config.dir, blockedFname)
+	config.rcFile = path.Join(config.dir, rcFname)
 }
 
 func loadBlocked(fpath string) (err error) {
 	f, err := os.Open(fpath)
 	if err != nil {
-		// No blocked domain file has no problem
-		return
+		// No blocked domain file has no problem, report other error though
+		if os.IsNotExist(err) {
+			return nil
+		} else {
+			errl.Println("Opening blocked file:", err)
+			return
+		}
 	}
+	defer f.Close()
 	fr := bufio.NewReader(f)
 
 	for {
@@ -54,7 +66,7 @@ func loadBlocked(fpath string) (err error) {
 		if err == io.EOF {
 			return nil
 		} else if err != nil {
-			errl.Printf("Error reading blocked domains %v", err)
+			errl.Println("Error reading blocked domains:", err)
 			return err
 		}
 		if domain == "" {
@@ -66,7 +78,69 @@ func loadBlocked(fpath string) (err error) {
 	return nil
 }
 
+func parseConfig() {
+	f, err := os.Open(config.rcFile)
+	if err != nil {
+		if err == os.ErrNotExist {
+			return
+		}
+		errl.Println("Opening config file:", err)
+	}
+	fr := bufio.NewReader(f)
+
+	var line string
+	var n int
+	for {
+		n++
+		line, err = ReadLine(fr)
+		if err == io.EOF {
+			return
+		} else if err != nil {
+			errl.Println("Error reading rc file:", err)
+			errl.Println("Exit")
+			os.Exit(1)
+		}
+
+		if line == "" {
+			continue
+		}
+
+		line = strings.TrimSpace(line)
+		// Ignore comment
+		if line[0] == '#' {
+			continue
+		}
+
+		v := strings.Split(line, "=")
+		if len(v) != 2 {
+			fmt.Println("Config error: syntax error on line", n)
+			os.Exit(1)
+		}
+		key, val := strings.TrimSpace(v[0]), strings.TrimSpace(v[1])
+
+		switch {
+		case key == "listen":
+			config.listenAddr = val
+		case key == "core":
+			config.numProc, err = strconv.Atoi(val)
+			if err != nil {
+				fmt.Printf("Config error: core number %d %v", n, err)
+				os.Exit(1)
+			}
+		case key == "socks":
+			config.socksAddr = val
+		case key == "blocked":
+			config.blockedFile = val
+		default:
+			fmt.Println("Config error: no such option", key)
+			os.Exit(1)
+		}
+	}
+}
+
 func loadConfig() {
+	parseConfig()
+
 	if err := loadBlocked(config.blockedFile); err != nil {
 		os.Exit(1)
 	}
