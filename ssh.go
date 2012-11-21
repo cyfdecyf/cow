596d9b6998dbdb711c94c2bdfad7dcb3bfc3ccd0
Chen Yufei <cyfdecyf@gmail.com>
2012-11-21 13:37:51 +0800

    ssh socks server liveness checking.
    Check if it's running before starting new one.

--- ssh.go
+++ ssh.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"net"
 	"os/exec"
 	"time"
 )
+
+func SshRunning() bool {
+	c, err := net.Dial("tcp", config.socksAddr)
+	if err != nil {
+		return false
+	}
+	c.Close()
+	return true
+}
 
 func runSSH() {
 	if config.sshServer == "" {
@@ -11,15 +21,28 @@ func runSSH() {
 	}
 
 	_, port := splitHostPort(config.socksAddr)
-	// -n redirects stdin from /dev/null
-	// -N do not execute remote command
+	alreadyRunPrinted := false
 
 	for {
+		if SshRunning() {
+			if !alreadyRunPrinted {
+				errl.Println("ssh socks server maybe already running, as cow can connect to",
+					config.socksAddr)
+				alreadyRunPrinted = true
+			}
+			// check server liveness in 1 minute
+			time.Sleep(60 * time.Second)
+			continue
+		}
+
+		// -n redirects stdin from /dev/null
+		// -N do not execute remote command
 		cmd := exec.Command("ssh", "-n", "-N", "-D", port, config.sshServer)
 		if err := cmd.Run(); err != nil {
 			errl.Println("ssh:", err)
 		}
 		info.Println("ssh exited, reconnect")
 		time.Sleep(5 * time.Second)
+		alreadyRunPrinted = false
 	}
 }
