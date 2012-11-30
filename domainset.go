4f97ff2b76921f78909ca8af3d18357175e51fb4
Chen Yufei <cyfdecyf@gmail.com>
2012-11-30 14:24:59 +0800

    Create config directory if not exist.

--- domainset.go
+++ domainset.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"io"
+	"log"
 	"os"
 	"path"
 	"sort"
@@ -211,20 +213,14 @@ func delDirectDomain(dm string) {
 }
 
 func writeBlockedDs() {
-	if !config.updateBlocked {
-		return
-	}
-	if !blockedDomainChanged {
+	if !config.updateBlocked || !blockedDomainChanged {
 		return
 	}
 	writeDomainList(config.blockedFile, blockedDs.toArray())
 }
 
 func writeDirectDs() {
-	if !config.updateDirect {
-		return
-	}
-	if !directDomainChanged {
+	if !config.updateDirect || !directDomainChanged {
 		return
 	}
 	writeDomainList(config.directFile, directDs.toArray())
@@ -296,7 +292,28 @@ func loadDomainList(fpath string) (lst []string, err error) {
 	return
 }
 
+func mkConfigDir() (err error) {
+	stat, err := os.Stat(config.dir)
+	if err == nil {
+		if stat.IsDir() {
+			return
+		}
+		log.Printf("%s is not directory, can't write domain list\n", config.dir)
+		return errors.New("config.dir is not directory")
+	}
+	if os.IsNotExist(err) {
+		err = os.Mkdir(config.dir, 0755)
+	}
+	if err != nil {
+		log.Printf("Config directory %s: %v\n", config.dir, err)
+	}
+	return
+}
+
 func writeDomainList(fpath string, lst []string) (err error) {
+	if err = mkConfigDir(); err != nil {
+		return
+	}
 	tmpPath := path.Join(config.dir, "tmp-domain")
 	f, err := os.Create(tmpPath)
 	if err != nil {
