1dbb7680710a857524faded69f1cacc4c146b163
Chen Yufei <cyfdecyf@gmail.com>
2012-12-14 21:41:09 +0800

    Use new shadowsocks interface.

--- proxy.go
+++ proxy.go
@@ -545,7 +545,7 @@ const connFailedErrCode = "504 Connection failed"
 func createParentProxyConnection(host string) (srvconn conn, err error) {
 	// Try shadowsocks server first
 	if hasShadowSocksServer {
-		if srvconn, err = createShadowSocksConnection(host); err == nil {
+		if srvconn, err = createShadowctSocksConnection(host); err == nil {
 			return
 		}
 	}
