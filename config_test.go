cc637bf2cdf1c6c7a49bb25958d5556a0f778fd8
Chen Yufei <cyfdecyf@gmail.com>
2013-01-28 21:07:29 +0800

    Add config test.

---
+++ config_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseListen(t *testing.T) {
+	parser := configParser{}
+	parser.ParseListen("127.0.0.1:8888")
+	if config.ListenAddr[0] != "127.0.0.1:8888" {
+		t.Error("single listen address parse error")
+	}
+
+	parser.ParseListen("127.0.0.1:8888, 127.0.0.1:7777")
+	if len(config.ListenAddr) != 2 {
+		t.Error("multiple listen address parse error")
+	}
+}
