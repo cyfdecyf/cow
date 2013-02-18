0cfa57cee91aa8ee652a86a60e324120bed4e1b1
Chen Yufei <cyfdecyf@gmail.com>
2013-02-18 13:30:11 +0800

    Add test for sendBodyChunked.

---
+++ proxy_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+)
+
+func TestSendBodyChunked(t *testing.T) {
+	testData := []struct {
+		raw string
+	}{
+		{"1a; ignore-stuff-here\r\nabcdefghijklmnopqrstuvwxyz\r\n10\r\n1234567890abcdef\r\n0\r\n\r\n"},
+		{"0\r\n\r\n"},
+	}
+
+	buf := make([]byte, bufSize)
+	for _, td := range testData {
+		r := bufio.NewReader(bytes.NewBufferString(td.raw))
+		var w bytes.Buffer
+
+		sendBodyChunked(buf, r, &w, nil)
+		if w.String() != td.raw {
+			t.Errorf("sendBodyChunked wrong, raw data is:\n%sgot:%s\n", td.raw, w.String())
+		}
+	}
+}
