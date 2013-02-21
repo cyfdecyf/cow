cyfdecyf/cow

commit 8d5e2c78003337acea65b18ee384ac10abe8c584

Do not tolerate single LF in chunked ending.
As this is not suggested in the spec.

proxy.go
@@ -968,7 +968,10 @@ func sendBodyChunked(buf []byte, r *bufio.Reader, w io.Writer) (err error) {
 			}
 			return
 		}
-		total := len(s) + int(size) // total data size for this chunk, not including ending CRLF
+		// The spec section 19.3 only suggest toleranting single LF for
+		// headers, so assume the server will send CRLF. If not, the following
+		// parse int may find errors.
+		total := len(s) + int(size) + 2 // total data size for this chunk, including ending CRLF
 		bn := r.Buffered()
 		left := total - bn
 		if left < 0 { // buffered content has more data than the current chunk
@@ -987,13 +990,7 @@ func sendBodyChunked(buf []byte, r *bufio.Reader, w io.Writer) (err error) {
 				debug.Println("Copying chunked data:", err)
 				return
 			}
-		} else {
-			if _, err = w.Write([]byte(CRLF)); err != nil {
-				debug.Println("Writing chunk ending CRLF:", err)
-				return
-			}
 		}
-		skipCRLF(r)
 	}
 	return
 }

proxy_test.go
@@ -13,11 +13,13 @@ func TestSendBodyChunked(t *testing.T) {
 		want string // empty means same as raw
 	}{
 		{"1a; ignore-stuff-here\r\nabcdefghijklmnopqrstuvwxyz\r\n10\r\n1234567890abcdef\r\n0\r\n\r\n", ""},
-		{"1a; ignore-stuff-here\nabcdefghijklmnopqrstuvwxyz\r\n10\n1234567890abcdef\n0\n\n",
-			// COW will only sanitize CRLF at chunk ending
-			"1a; ignore-stuff-here\nabcdefghijklmnopqrstuvwxyz\r\n10\n1234567890abcdef\r\n0\r\n\r\n"},
 		{"0\r\n\r\n", ""},
-		{"0\n\r\n", "0\r\n\r\n"}, // test for buggy web servers
+		/*
+			{"0\n\r\n", "0\r\n\r\n"}, // test for buggy web servers
+			{"1a; ignore-stuff-here\nabcdefghijklmnopqrstuvwxyz\r\n10\n1234567890abcdef\n0\n\n",
+				// COW will only sanitize CRLF at chunk ending
+				"1a; ignore-stuff-here\nabcdefghijklmnopqrstuvwxyz\r\n10\n1234567890abcdef\r\n0\r\n\r\n"},
+		*/
 	}
 
 	buf := make([]byte, bufSize)

commit b57b8319424a311d694186fce467056fcaa3af23

Fix extra CRLF bug in sendBodyChunked.
This regression bug is introduced in commit 8d5e2c7.
Also add test case for this bug.

proxy.go
@@ -986,7 +986,7 @@ func sendBodyChunked(buf []byte, r *bufio.Reader, w io.Writer) (err error) {
 			return
 		}
 		if left > 0 {
-			if err = copyN(r, w, left, buf, nil, []byte(CRLF)); err != nil {
+			if err = copyN(r, w, left, buf, nil, nil); err != nil {
 				debug.Println("Copying chunked data:", err)
 				return
 			}

proxy_test.go
@@ -23,21 +23,26 @@ func TestSendBodyChunked(t *testing.T) {
 	}
 
 	buf := make([]byte, bufSize)
-	for _, td := range testData {
-		r := bufio.NewReaderSize(strings.NewReader(td.raw), bufSize)
-		var w bytes.Buffer
+	// use different reader buffer size to test for both all buffered and partially buffered chunk
+	sizeArr := []int{32, 64, 128}
+	for _, size := range sizeArr {
+		for _, td := range testData {
+			r := bufio.NewReaderSize(strings.NewReader(td.raw), size)
+			var w bytes.Buffer
 
-		if err := sendBodyChunked(buf, r, &w); err != nil {
-			t.Fatal("err:", err)
-		}
-		if td.want == "" {
-			if w.String() != td.raw {
-				t.Errorf("sendBodyChunked wrong, raw data is:\n%q\ngot:\n%q\n", td.raw, w.String())
+			if err := sendBodyChunked(buf, r, &w); err != nil {
+				t.Fatal("err:", err)
 			}
-		} else {
-			if w.String() != td.want {
-				t.Errorf("sendBodyChunked wrong, raw data is:\n%q\nwant:\n%q\ngot :\n%q\n",
-					td.raw, td.want, w.String())
+			if td.want == "" {
+				if w.String() != td.raw {
+					t.Errorf("sendBodyChunked wrong with buf size %d, raw data is:\n%q\ngot:\n%q\n",
+						size, td.raw, w.String())
+				}
+			} else {
+				if w.String() != td.want {
+					t.Errorf("sendBodyChunked wrong with buf sizwe %d, raw data is:\n%q\nwant:\n%q\ngot :\n%q\n",
+						size, td.raw, td.want, w.String())
+				}
 			}
 		}
 	}