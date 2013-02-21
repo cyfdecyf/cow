8c6f9a639cde09ac446460e5eaa4c7054a44791e
Chen Yufei <cyfdecyf@gmail.com>
2013-02-21 09:52:46 +0800

    Add sendBodyChunked test case for buggy http server.

--- proxy_test.go
+++ proxy_test.go
@@ -9,10 +9,15 @@ import (
 
 func TestSendBodyChunked(t *testing.T) {
 	testData := []struct {
-		raw string
+		raw  string
+		want string // empty means same as raw
 	}{
-		{"1a; ignore-stuff-here\r\nabcdefghijklmnopqrstuvwxyz\r\n10\r\n1234567890abcdef\r\n0\r\n\r\n"},
-		{"0\r\n\r\n"},
+		{"1a; ignore-stuff-here\r\nabcdefghijklmnopqrstuvwxyz\r\n10\r\n1234567890abcdef\r\n0\r\n\r\n", ""},
+		{"1a; ignore-stuff-here\nabcdefghijklmnopqrstuvwxyz\r\n10\n1234567890abcdef\n0\n\n",
+			// COW will only sanitize CRLF at chunk ending
+			"1a; ignore-stuff-here\nabcdefghijklmnopqrstuvwxyz\r\n10\n1234567890abcdef\r\n0\r\n\r\n"},
+		{"0\r\n\r\n", ""},
+		{"0\n\r\n", "0\r\n\r\n"}, // test for buggy web servers
 	}
 
 	buf := make([]byte, bufSize)
@@ -23,8 +28,15 @@ func TestSendBodyChunked(t *testing.T) {
 		if err := sendBodyChunked(buf, r, &w); err != nil {
 			t.Fatal("err:", err)
 		}
-		if w.String() != td.raw {
-			t.Errorf("sendBodyChunked wrong, raw data is:\n%q\ngot:\n%q\n", td.raw, w.String())
+		if td.want == "" {
+			if w.String() != td.raw {
+				t.Errorf("sendBodyChunked wrong, raw data is:\n%q\ngot:\n%q\n", td.raw, w.String())
+			}
+		} else {
+			if w.String() != td.want {
+				t.Errorf("sendBodyChunked wrong, raw data is:\n%q\nwant:\n%q\ngot :\n%q\n",
+					td.raw, td.want, w.String())
+			}
 		}
 	}
 }
