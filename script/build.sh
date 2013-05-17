bc6932bc5248f8801cbeac7fe6e31c206ae634a9
Chen Yufei <cyfdecyf@gmail.com>
2013-05-17 11:12:40 +0800

    Add sample rc in windows zip bundle.

--- script/build.sh
+++ script/build.sh
@@ -19,8 +19,9 @@ build() {
     if [[ $1 == "windows" ]]; then
         mv cow.exe script
         pushd script
-        zip $name.zip cow.exe cow-taskbar.exe
-        rm -f cow.exe
+        cp ../doc/sample-config/rc sample-rc.txt
+        zip $name.zip cow.exe cow-taskbar.exe sample-rc.txt
+        rm -f cow.exe sample-rc.txt
         mv $name.zip ../bin/
         popd
     else 
