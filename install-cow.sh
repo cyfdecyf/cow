c844dfce6a31ce3a5092e357a5e9e23e2996adba
Wang Xinyu <comicfans44@gmail.com>
2015-05-18 09:36:05 +0800

    install script: improve detect arm without vfp

--- install-cow.sh
+++ install-cow.sh
@@ -11,7 +11,14 @@ case $arch in
         arch="32"
         ;;
     "armv5tel" | "armv6l" | "armv7l")
-        arch="-$arch"
+        features=`cat /proc/cpuinfo | grep Features`
+        if [[ ! "$features" =~ "vfp" ]]; then
+            #arm without vfp must use GOARM=5 binary
+            #see https://github.com/golang/go/wiki/GoArm
+            arch="-armv5tel"
+        else
+            arch="-$arch"
+        fi
         ;;
     *)
         echo "$arch currently has no precompiled binary"
