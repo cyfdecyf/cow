fd8203d39cd8a66183b93d53c4b4fc73291c207e
Chen Yufei <cyfdecyf@gmail.com>
2015-05-19 22:36:57 +0800

    Update README-en.md in set version script.

--- script/set-version.sh
+++ script/set-version.sh
@@ -13,4 +13,4 @@ version=$1
 sed -i -e "s,\(\tversion \+= \)\".*\"$,\1\"$version\"," config.go
 sed -i -e "s/version=.*$/version=$version/" install-cow.sh
 sed -i -e "s/当前版本：[^ ]\+ \(.*\)\$/当前版本：$version \1/" README.md
-
+sed -i -e "s/Current version: [^ ]\+ \(.*\)\$/Current version: $version \1/" README-en.md
