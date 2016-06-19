1f2130faecabdc5cdacfd439a68570137b51f18c
Chen Yufei <cyfdecyf@gmail.com>
2016-06-19 23:47:22 +0800

    Bump version to 0.9.8

--- config.go
+++ config.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	version               = "0.9.7"
+	version               = "0.9.8"
 	defaultListenAddr     = "127.0.0.1:7777"
 	defaultEstimateTarget = "example.com"
 )
--- install-cow.sh
+++ install-cow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=0.9.7
+version=0.9.8
 
 arch=`uname -m`
 case $arch in
