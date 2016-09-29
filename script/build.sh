a1c1c2095801964401d7f051abfbb69aa2f86824
renzhn <renzhen999@gmail.com>
2016-09-29 09:13:44 +0000

    fix build script

--- script/build.sh
+++ script/build.sh
@@ -8,6 +8,7 @@ version=`grep '^version=' ./install.sh | sed -s 's/version=//'`
 go_version=`go version`
 echo "creating MEOW binary version $version"
 
+rm -rf bin
 mkdir -p bin/windows
 
 gox -output="bin/{{.Dir}}-{{.OS}}-{{.Arch}}-$version" -os="windows"
@@ -63,6 +64,6 @@ git checkout gh-pages
 rm -rf dist
 mv bin dist
 git add dist
-git commit -m"version $version $go_version"
-git push
+git commit -am"version $version $go_version"
+git push -f
 git checkout master
